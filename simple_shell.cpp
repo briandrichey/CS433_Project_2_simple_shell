933b844dda5105a8361b611a3fda0173d6036e39
Brian D. Richey <62313053+briandrichey@users.noreply.github.com>
2021-10-13 17:50:19 -0700

    Update simple_shell.cpp

--- simple_shell.cpp
+++ simple_shell.cpp
@@ -58,6 +58,7 @@ void simple_shell::tokenize(char command[], char* args[]) {
 
 void simple_shell::execute(char* args[]) {
 	string str_args(args[0]);
+	int fd [2];
 
 	if (str_args == "exit") {
 		shouldRun = false;
@@ -83,6 +84,46 @@ void simple_shell::execute(char* args[]) {
 			
 			
 		}
+	if(str_args == "|"){
+		//translate this to our code
+		/* create the pipe */
+   if (pipe(fd) == ‐1) {
+     fprintf(stderr,"Pipe failed");
+     return 1;
+   }
+
+   /* fork a child process */
+   pid = fork();
+
+   if (pid < 0) { /* error occurred */
+     fprintf(stderr, "Fork Failed");
+     return 1;
+   }
+
+   if (pid > 0) { /* parent process */
+     /* close the unused end of the pipe */
+     close(fd[READ_END]);
+
+     /* write to the pipe */
+     write(fd[WRITE_END], write_msg, strlen(write_msg)+1);
+
+     /* close the write end of the pipe */
+     close(fd[WRITE_END]);
+   }
+   else { /* child process */
+     /* close the unused end of the pipe */
+     close(fd[WRITE_END]);
+
+     /* read from the pipe */
+     read(fd[READ_END], read_msg, BUFFER_SIZE);
+     printf("read %s",read_msg);
+
+     /* close the read end of the pipe */
+     close(fd[READ_END]);
+   }
+   return 0;
+}
+	}
 
 	//fork the process
 	pid_t pid = fork();
