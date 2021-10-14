541892797c3f430d97799c4baeaa3f17f6ce721f
Brian D. Richey <62313053+briandrichey@users.noreply.github.com>
2021-10-13 17:00:46 -0700

    Update simple_shell.cpp

--- simple_shell.cpp
+++ simple_shell.cpp
@@ -1,1 +1,101 @@
+#include "simple_shell.h"
+#include <stdio.h>
+#include <cstdlib>
+#include <iostream>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
+
+
+simple_shell::simple_shell() {
+	shouldRun = true;
+	shouldWait = false;
+}
+
+void simple_shell::displayHistory() {
+	for (int i = 0; i < commandHistory.size(); i++) {
+		cout << "Command history: " << i << " " << commandHistory[i] << endl;
+	}
+}
+
+
+void simple_shell::tokenize(char command[], char* args[]) {
+	int i = 0;
+	bool shouldSave = true;
+	char* token = strtok(command, " ");
+
+	while (token != NULL) {
+		string str_token(token);
+
+		if (str_token == "!!") {
+			shouldSave = false;
+		}
+		else if (str_token == "exit") {
+			shouldSave = false;
+		}
+		else if (str_token == "&") {
+			shouldSave = false;
+			shouldWait = true;
+		}
+		
+		args[i] = token;
+		i = i + 1;
+		token = strtok(NULL, " ");
+	}
+	args[i] = NULL;
+
+
+	if (shouldSave == true) {
+		//displayHistory();
+		for (int i = 0; i < strlen(*args); i++) {
+			//cout << args[i] << endl;
+			commandHistory.push_back(args[i]);
+		}
+	}
+}
+
+
+void simple_shell::execute(char* args[]) {
+	string str_args(args[0]);
+
+	if (str_args == "exit") {
+		shouldRun = false;
+		return;
+	}
+
+	/*history functionality*/
+	if (str_args == "!!") {
+		if (!commandHistory.empty()) {
+			strcpy(*args, commandHistory.back()); 		
+			commandHistory.pop_back();
+		}
+		else {
+			cout << "No commands in history!" << endl;
+			return;
+		}
+	}
+
+	//fork the process
+	pid_t pid = fork();
+
+	//child process will do execvp.
+	if (pid == 0) {
+		execvp(args[0], args);		//execvp doesnt return unless there is issue
+
+		exit(1);	//this will only get hit if there is an issue with a command
+	}
+	//parent process will wait if there is an &
+	else if (pid > 0) {
+		if (shouldWait == true) {
+			wait(NULL);
+		}
+		wait(NULL);		//found this line from the wait man page
+		
+	}
+	else {
+		cout << "ERROR FORKING" << endl;
+		shouldRun = false;
+		exit(1);
+	}
+}
