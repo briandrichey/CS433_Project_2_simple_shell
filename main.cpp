99de8a49efbb100bb7947355ea9eb028c8b32089
Brian D. Richey <62313053+briandrichey@users.noreply.github.com>
2021-10-13 17:52:57 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+#include "simple_shell.h"
 
+#define READ_END 0 
+#define WRITE_END 1
+
+int main(void) {
+	simple_shell osh; //sets shouldRun to true on creation
+
+	/*
+	* After reading user input, steps are: 
+	* 1- fork a child process using fork()
+	* 2- the child process will invoke execvp()
+	* 3- parent will invoke wait() UNLESS the command included &
+	*/
+	while (osh.shouldRun) {						/*!DONT FORGET TO SET OSH.SHOULDRUN TO FALSE, EVENTUALLY!*/
+		char commands[MAX_LINE];
+		char * args[MAX_LINE / 2 + 1];
+    int fd [2];
+		
+		cout << "osh> ";
+		cout.flush();
+
+		cin.getline(commands, MAX_LINE);
+
+		osh.tokenize(commands, args);	//this constructs args[] for execvp
+
+		osh.execute(args); //this calls fork and execvp
+
+	}
+
+	return 0;
+}
